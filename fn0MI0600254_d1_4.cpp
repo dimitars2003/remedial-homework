9a506297202cea888a24d575a96768ef3c1ad96c
dimitars2003 <72808611+dimitars2003@users.noreply.github.com>
2023-08-24 21:37:30 +0300

    Add files via upload

---
+++ fn0MI0600254_d1_4.cpp
@@ -0,0 +1,22 @@
+/**
+*
+* Solution to homework assignment 9
+* Introduction to programming course
+* Faculty of Mathematics and Informatics of Sofia University
+* Winter semester 2022/2023
+*
+* @author Dimitar Donkov
+* @idnumber 0MI0600254
+* @task 1
+*
+*/
+
+#include <iostream>
+
+using namespace std;
+
+int main () {
+
+    return 0;
+}
+
